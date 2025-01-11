7f438d2fdec158a862e858f5f2c3542f4cb4d7c8
Owen <31156070+imowen@users.noreply.github.com>
2025-01-11 16:53:31 +0700

    Update watermark.tsx

--- components/templates/elements/watermark.tsx
+++ components/templates/elements/watermark.tsx
@@ -1,1 +1,27 @@
+import { absoluteUrl } from "@/lib/url"
 
+interface Props {
+  style?: React.CSSProperties
+}
+
+export function Watermark({ style }: Props) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        position: "absolute",
+        ...style,
+      }}
+    >
+      <img
+        src={absoluteUrl("/assets/watermark.svg")}
+        alt="imgsrc.io"
+        style={{
+          // watermark svg has a 2.7 aspect ratio
+          width: "0rem",
+          height: "0rem",
+        }}
+      />
+    </div>
+  )
+}
